569ba68431ddcf281c21db6cb595bd1e59b49251
gkaracsonyi <gkaracso@gmail.com>
2024-03-01 14:34:31 -0500

    import pages. import comps for dev. add routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import Home from './views/Home.jsx';
+import About from './views/About.jsx';
+
+// DEVELOPER //
+
+import Header from './views/components/Header.jsx';
+import Footer from './views/components/Header.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='about' element={<About />} />
+                {/* DEVELOPER */}
+                <Route path='header' element={<Header />} />
+                <Route path='footer' element={<Footer />} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
+);
